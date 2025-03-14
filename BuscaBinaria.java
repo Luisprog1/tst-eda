0ed57558e285e2e44edb85a3f5aaa05a6ad91d43
luisprog1 <luis.alberto.viana.dantas@ccc.ufcg.edu.br>
2025-03-14 18:09:08 -0300

    Busca Binaria iterativa

---
+++ BuscaBinaria.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+class BuscaBinaria {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        String[] seq = sc.nextLine().split(" ");
+        int chave = sc.nextInt();
+        System.out.println(buscaBinaria(seq, 0, seq.length-1, chave));
+    }
+    public static int buscaBinaria(String[] seq, int ini, int fim, int key){
+        while(ini <= fim){
+            int meio = (ini+fim) / 2;
+            if(Integer.parseInt(seq[meio]) == key ){
+                return meio;
+            }
+
+            else if(Integer.parseInt(seq[meio]) > key){
+                System.out.println(meio);
+                fim = meio -1;
+            }
+
+            else{
+                System.out.println(meio);
+                ini = meio + 1;
+            }
+        }
+
+        return -1;
+    }
+}
---
+++ Predecessor_bst.java
@@ -0,0 +1,4 @@
+package PACKAGE_NAME;
+
+public class Predecessor_bst {
+}
