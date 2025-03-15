c7b816ed941c4887cd8fd5274fd47a89f3f8dda1
luisprog1 <luis.alberto.viana.dantas@ccc.ufcg.edu.br>
2025-03-15 14:11:53 -0300

    Busca Linear Recursiva

---
+++ BuscaLinearRecursiva.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class BuscaLinearRecursiva {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        String[] seq = sc.nextLine().split(" ");
+        int chave = sc.nextInt();
+        System.out.println(buscaLinearRecursiva(seq, chave, 0));
+    }
+    public static int buscaLinearRecursiva(String[] seq, int key, int index){
+        if(index < seq.length){
+            if(Integer.parseInt(seq[index]) == key)
+                return index;
+            else
+                return buscaLinearRecursiva(seq, key, index + 1);
+        }
+        return -1;
+
+    }
+}
