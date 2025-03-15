70fafb5be345da13353251f9426321ca308ea08e
luisprog1 <luis.alberto.viana.dantas@ccc.ufcg.edu.br>
2025-03-15 14:19:41 -0300

    Encontra Primeiro Negativo

---
+++ EPN.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class EPN {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        String[] seq = sc.nextLine().split(" ");
+        String primeiro = epn(seq, 0);
+        System.out.println(primeiro);
+    }
+
+    public static String epn(String[] seq, int index){
+        if(index < seq.length){
+
+            if(Integer.parseInt(seq[index]) < 0){
+                return seq[index];
+
+            }
+
+            return epn(seq, index+1);
+        }
+        return "-";
+    }
+}
